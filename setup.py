57f004e05d461ef4aad6203ad35c033dfea7eefa
IguanaAzul <ramon.griffo98@gmail.com>
2020-11-17 16:49:03 -0300

    setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='rubiks_cube',
+    version='1.0.0',
+    description='A Rubiks Cube Package for Python',
+    url='https://github.com/IguanaAzul/RubiksCube',
+    author='Ramon Griffo',
+    author_email='ramon.griffo98@gmail.com',
+    license='MIT License',
+    packages=['Cube'],
+    install_requires=['numpy'],
+)
